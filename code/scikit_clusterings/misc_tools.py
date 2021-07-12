f2dcaf6cbd77e5322905b2adfe0bd68445469ed1
Ryoko <ryokonoda.1811@gmail.com>
2021-07-12 15:19:21 +0300

    Added tiny evaluation function to misc_tools.py
    Added a function that prints and returns adjusted mutual info score and Fowlkes-Mallows score. Tested it on scikit_kmeans.ipynb so both files have changed.

--- code/scikit_clusterings/misc_tools.py
+++ code/scikit_clusterings/misc_tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn import preprocessing
+from sklearn.metrics import adjusted_mutual_info_score,fowlkes_mallows_score
 
 def le_family_names(m1data):
     """
@@ -19,3 +20,27 @@ def le_family_names(m1data):
         print(f"{label}: {le.transform(label.reshape(-1))}")
         
     return family_names, family_codes
+    
+
+def ami_and_fmscore(true_labels, predicted_labels, silent=False):
+    """
+    This function will print the adjusted mutual information and Fowlkes-Mallows score
+    while returning the two values.
+    Setting silent=True will prevent the messages from printing.
+    """
+    # unadjusted mutual information score measures if two cluster assignements agree with each other
+    # the unadjusted score gives us false results when cluster sizes are small, so using adjusted version
+    # value=1: perfect, value around 0: same as random assignment (can be negative)
+    AMI = adjusted_mutual_info_score(true_labels, predicted_labels)
+    if silent==False:
+        print("Adjusted Mutual Info: {}".format(AMI))
+        print("(0: bad, 1: perfect)\n")
+
+    # the Fowlkes-Mallows score geometric mean between of the precision and recall
+    # value=1: perfect, value=0: terrible
+    FMS = fowlkes_mallows_score(true_labels, predicted_labels)
+    if silent==False:
+        print("Fowlkes-Mallows Score:  {}".format(FMS))
+        print("(0: bad, 1: perfect)\n")
+    
+    return AMI, FMS
